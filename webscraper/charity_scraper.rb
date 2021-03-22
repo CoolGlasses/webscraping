4e3533ce5059609ec15e701326fd71058fb74427
CoolGlasses <robinandrea_2@yahoo.com>
2021-03-21 19:10:30 -0700

    first draft of the charity_scraper completed

--- webscraper/charity_scraper.rb
+++ webscraper/charity_scraper.rb
@@ -11,12 +11,35 @@ CSV.foreach("datadotgov_main.csv", headers: true, header_converters: :symbol) do
     table << row
 end
 
-#search for the specific charity table[row #][:abn]
-#populate the search form with the first ABN, click on search
-#click on the charity to open the main page of the charity
-#grab total income from main page -- assign to table[row #][:income]
-#navigate to People tab
-#scrape all people listed -- assign to table[row #][:person_#{number}]
+table.each do |row|
+    #go to website
+    agent = Mechanize.new
+    search_page = agent.get('https://www.acnc.gov.au/charity')
+
+    #search for the specific charity table[row #][:abn]
+    #populate the search form with the first ABN, click on search
+    search_form = search_page.forms[1]
+    search_form.name_abn = row[:abn] #need to adjust for iteration through table!
+    results_page = agent.submit(search_form, search_form.buttons.first)
+
+    #click on the charity to open the main page of the charity
+    charity_main_page = results_page.link_with(text: row[:charity_legal_name]).click #need to adjust for iteration through table!
+
+    #grab total income from main page -- assign to table[row #][:income]
+    income = charity_main_page.css('.field-name-acnc-node-charity-graphs > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)').text
+    income = income[13..-1]
+    row[:income] = income #need to fix for table iteration!
+
+    #navigate to People tab
+    #scrape all people listed -- assign to table[row #][:person_#{number}]
+    people = charity_main_page.css('#views-bootstrap-grid-1 > div:nth-child(1)')
+    people = people.css('h4')
+    people.each_with_index do |person, i| #need to adjust for iteration through table!
+        row["person_#{i + 1}".to_sym] = person.text
+    end
+end
+
+
 #write scraped information to the csv file (can this be done via iteration through each row of CSV?)
 
 headers = table.first.headers
